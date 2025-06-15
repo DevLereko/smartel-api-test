488196f514500b310a5d0a9ce7840ad30c349e14
DevLereko <folokolereko@gmail.com>
2025-06-15 13:00:38 +0200

    Added routes for task management

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router();
 import authController from "../controllers/authentication/authView";
+import taskView from "../controllers/task-management/taskView";
 import verifySignUp from "../middlewares/verifySignUp";
 
 router.post("/auth/login", authController.signin);
@@ -10,6 +11,13 @@ router.post(
   [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted],
   authController.createUser
 );
+
+router.get("/tasks", taskView.getAllTasksHandler);
+router.get("/tasks/:id", taskView.getTaskHandler);
+router.post("/tasks", taskView.createTaskHandler);
+router.put("/tasks/:id", taskView.updateTaskHandler);
+router.delete("/tasks/:id", taskView.deleteTaskHandler);
+
 module.exports = router;
 
 export = router;
