a5e960c8a52c4a2a4f47d1f6077f2cb47ab03ab2
DevLereko <folokolereko@gmail.com>
2025-06-15 11:19:00 +0200

    Added role model

---
+++ src/models/role.model.ts
@@ -0,0 +1,24 @@
+module.exports = (
+  sequelize: {
+    define: (
+      arg0: string,
+      arg1: {
+        id: { type: number; primaryKey: boolean };
+        name: { type: string };
+      }
+    ) => any;
+  },
+  Sequelize: { INTEGER: number; STRING: string }
+) => {
+  const Role = sequelize.define("roles", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+    },
+  });
+
+  return Role;
+};
