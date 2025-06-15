3c0a1a51a585d8a725c9acb93ae1a204700c5ebc
Lereko Foloko <folokolereko@gmail.com>
2025-06-15 20:06:47 +0200

    Added an endpoint for retrieving all users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -13,6 +13,8 @@ router.post(
   authController.createUser
 );
 
+router.get("/auth/users", [authJwt.verifyToken], authController.getAllUsers);
+
 router.get("/tasks", [authJwt.verifyToken], taskView.getAllTasksHandler);
 router.get("/tasks/:id", [authJwt.verifyToken], taskView.getTaskHandler);
 router.post("/tasks", [authJwt.verifyToken], taskView.createTaskHandler);
