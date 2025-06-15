50e55af4d238c484e828a71c54645ad4f1c2e0c3
DevLereko <folokolereko@gmail.com>
2025-06-15 13:10:53 +0200

    Added authJWT middleware and used it in routes for some endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import authController from "../controllers/authentication/authView";
 import taskView from "../controllers/task-management/taskView";
 import verifySignUp from "../middlewares/verifySignUp";
+import authJwt from "../middlewares/authJwt";
 
 router.post("/auth/login", authController.signin);
 
@@ -12,11 +13,11 @@ router.post(
   authController.createUser
 );
 
-router.get("/tasks", taskView.getAllTasksHandler);
-router.get("/tasks/:id", taskView.getTaskHandler);
-router.post("/tasks", taskView.createTaskHandler);
-router.put("/tasks/:id", taskView.updateTaskHandler);
-router.delete("/tasks/:id", taskView.deleteTaskHandler);
+router.get("/tasks", [authJwt.verifyToken], taskView.getAllTasksHandler);
+router.get("/tasks/:id", [authJwt.verifyToken], taskView.getTaskHandler);
+router.post("/tasks", [authJwt.verifyToken], taskView.createTaskHandler);
+router.put("/tasks/:id", [authJwt.verifyToken], taskView.updateTaskHandler);
+router.delete("/tasks/:id", [authJwt.verifyToken], taskView.deleteTaskHandler);
 
 module.exports = router;
 
